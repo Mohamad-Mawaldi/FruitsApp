f06cacc962540bd2b9a1770217c0cf58f0c5f6cb
Mohamad <ima.mawaldi@gmail.com>
2023-10-16 22:26:19 +0200

    Initial Commit

---
+++ FruitsApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FruitsApp
+//
+//  Created by mohamad on 2023-10-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var 
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FruitsApp/FruitsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsAppApp.swift
+//  FruitsApp
+//
+//  Created by mohamad on 2023-10-16.
+//
+
+import SwiftUI
+
+@main
+struct FruitsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
